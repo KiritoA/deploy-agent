d1d74ac8e09b3d8a8716a253a1a95fecf9df1648
KiritoA <jason_dx@outlook.com>
2020-05-17 14:39:59 +0800

    Registry auth from previous spec

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"context"
 	"crypto/subtle"
-	"encoding/base64"
-	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/docker/docker/api/types"
@@ -132,17 +130,8 @@ func update(writer http.ResponseWriter, request *http.Request) {
 	serviceSpec := &service.Spec
 	serviceSpec.TaskTemplate.ContainerSpec.Image = imageName
 
-	authConfig := types.AuthConfig{
-		ServerAddress: config.Registry,
-		Username:      request.FormValue("username"),
-		Password:      request.FormValue("password"),
-	}
-
-	authBytes, _ := json.Marshal(authConfig)
-	encodedAuthConfig := base64.URLEncoding.EncodeToString(authBytes)
-
 	updateResp, err := dockerClient.ServiceUpdate(context.Background(), serviceName, service.Version, *serviceSpec,
-		types.ServiceUpdateOptions{EncodedRegistryAuth: encodedAuthConfig})
+		types.ServiceUpdateOptions{RegistryAuthFrom: "previous-spec"})
 	if err != nil {
 		logger.Error("Failed to update service: ", err)
 		writer.WriteHeader(http.StatusInternalServerError)
